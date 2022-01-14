19a8eaa9196578c787c271bc9163c15296ba9549
Khaled <74397286+khaled-0@users.noreply.github.com>
2022-01-14 13:20:47 +0000

    MAYK

--- functions/mike.js
+++ functions/mike.js
@@ -3,6 +3,9 @@ module.exports = {
   description: "Mike is mongass sus",
   async execute(message) {
     if (!message.content.toLowerCase().includes("mike")) return;
-    await message.react("<a:mongass:872257017018982461>").catch(console.error);
+    await message.react(":regional_indicator_m: ").catch(console.error);
+    await message.react(":regional_indicator_i: ").catch(console.error);
+    await message.react(":regional_indicator_k: ").catch(console.error);
+    await message.react(":regional_indicator_e: ").catch(console.error);
   }
 };
