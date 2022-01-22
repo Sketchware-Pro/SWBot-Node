4a2b32815999efb98cd5c1e6e2811c93b5a668a9
Khaled <74397286+khaled-0@users.noreply.github.com>
2022-01-22 16:01:18 +0000

    use regex instead of string.match in petition

--- functions/petition.js
+++ functions/petition.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "For holding petitions I guess",
   async execute(message) {
     if (message.content.length < 8) return;
-    if (!message.content.toLowerCase().includes("petition")) return;
+    if (!message.content.match(/\b(petition)\b/i)) return;
     await message.react("<:upvote:833702317098008646>").catch(console.error);
     await message.react('<:downvote:833702170306150440>').catch(console.error);
   }
