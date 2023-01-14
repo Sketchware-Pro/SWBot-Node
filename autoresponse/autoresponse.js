ab43c3e1bd8cf3c262b688f16a24beb78135736f
Khaled <spkhalad@gmail.com>
2023-01-14 13:43:32 +0600

    Restructure autoresponses.

--- autoresponse/autoresponse.js
+++ autoresponse/autoresponse.js
@@ -1,61 +1,84 @@
-const fs = require('fs');
+const fetch = require("node-fetch");
 const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
-let dataPath = "./autoresponse/_data.json"
-let autoResponsesList = updateAutoResopnseData();
+let autoResponsesList;
 var responseTimeout = 60000; //1min
 
-function updateAutoResopnseData() {
-  fs.readFile(dataPath, (err, data) => {
-    if (err) return console.error(err);
-    try {
-      autoResponsesList = JSON.parse(data);
-      console.log('Autoresponse data loaded');
-    } catch (e) {
-      return console.error(e);
-    }
-  });
+async function updateAutoResopnseData() {
+  try {
+    const autoResponseDataUrl =
+      "https://raw.githubusercontent.com/khaled-0/SWBot-Node/main/autoresponse/AutoResponseData.json";
+
+    const response = await fetch(autoResponseDataUrl);
+    const AutoResponseDataJSON = await response.text();
+    autoResponsesList = JSON.parse(AutoResponseDataJSON);
+    console.log("Autoresponse data loaded");
+    return true;
+  } catch (error) {
+    console.log("Autoresponse data error. " + error.message);
+    return error;
+  }
 }
+updateAutoResopnseData();
 
 module.exports = {
   async execute(message) {
-    let embed = new MessageEmbed()
-      .setColor("#202225");
+    //Don't reply to bots
+    if (message.author.bot) return;
+
+    let embed = new MessageEmbed().setColor("#202225");
 
     /**
-     * Check if message is a command and execute (only from mods & admins) 
+     * Check if message is a command and execute (only from mods & admins)
      *
      */
-    if ((message.content.startsWith("+autoresponse"))
-      && (message.member.permissions.has('MANAGE_MESSAGES'))) {
-
-      //Reload data from file (unnecessary)
+    if (
+      message.content.startsWith("+autoresponse") &&
+      message.member.permissions.has("MANAGE_MESSAGES")
+    ) {
+      //Reload data manually
       if (message.content == "+autoresponse reload") {
-        updateAutoResopnseData()
-        return await message.reply("Autoresponses reloaded")
+        let autoresponseLoaded = await updateAutoResopnseData();
+        if (!autoresponseLoaded) {
+          return await message.reply(
+            "Autoresponse loading failed. " + autoresponseLoaded.message
+          );
+        }
+        return await message.reply("Autoresponses reloaded");
       }
+      require("./listall").execute(message, autoResponsesList);
     }
 
     /* ************************************************************* */
 
     /**
-    * Match the message and reply
-    */
+     * Match the message and reply
+     */
 
     let messageBody = message.content.replace("\n", "");
     for (let keyword in autoResponsesList) {
-      let pattern = new RegExp(keyword, 'i');
+      let pattern = new RegExp(keyword, "i");
       if (messageBody.match(pattern)) {
-
         //Check if theres an ignore list and if the current channel is in it then return
-        if (autoResponsesList[keyword].ignoredChannels && autoResponsesList[keyword].ignoredChannels.includes(message.channel.id)) return;
+        if (
+          autoResponsesList[keyword].ignoredChannels &&
+          autoResponsesList[keyword].ignoredChannels.includes(
+            message.channel.id
+          )
+        )
+          return;
 
-        let responseContent = autoResponsesList[keyword].response.replaceAll("%author", `<@${message.author.id}>`) //.replaceAll(/{#(.*?)}/gm, "<#$1>")
+        let responseContent = autoResponsesList[keyword].response.replaceAll(
+          "%author",
+          `<@${message.author.id}>`
+        );
         if (responseContent.startsWith("{") && responseContent.endsWith("}")) {
-          embed.setDescription(responseContent.substr(1, (responseContent.length - 2)))
-          return await message.reply({ embeds: [embed] })
+          embed.setDescription(
+            responseContent.substr(1, responseContent.length - 2)
+          );
+          return await message.reply({ embeds: [embed] });
         }
-        return await message.reply(responseContent)
+        return await message.reply(responseContent);
       }
     }
-  }
-}
+  },
+};
---
+++ autoresponse/listall.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  async execute(message, autoResponsesList) {
+    //Get list of all autoresponses
+    if (message.content != "+autoresponse list") return;
+    let embed = new MessageEmbed();
+    var n = 0;
+    for (let ares in autoResponsesList) {
+      embed.addField(
+        `**${n}. ${ares}**`,
+        `${autoResponsesList[ares].response} ${
+          autoResponsesList[ares].ignoredChannels
+            ? "\n\n**Ignored in:** " + autoResponsesList[ares].ignoredChannels
+            : ""
+        }`,
+        true
+      );
+      n++;
+    }
+    embed.setTitle("List Of All Automated Responses");
+    return await message.reply({ embeds: [embed] });
+  },
+};
